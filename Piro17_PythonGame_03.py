818816b8e279925b988371dac477f17138ecae40
Yeonseo Jo <jys073190@gmail.com>
2022-07-11 03:22:25 +0900

    게임4 두부게임 구현 완료

--- Piro17_PythonGame_03.py
+++ Piro17_PythonGame_03.py
@@ -1,3 +1,4 @@
+from bdb import Breakpoint
 import random
 
 class Player:
@@ -243,45 +244,99 @@ class Game:
 
     def game_4(self):
       '''술게임 4'''
-      # TODO 6
-      reaction = ["캌 퉤", "나도 좋아"]
-      now = self.player[len(self.player) - 1].name
-      name = input("술도 마셨는데 좋아게임할까? ")[0:2]
-      cnt = 0
+      # [두부 게임 설명]
+      # 1)참가자+ 두부n개 = 총 5개가 리스트 안에서 랜덤으로 섞여 배열을 만든다
+      # 3) 두부 3모를 기준으로 왼쪽으로 갈 수록 1모씩 모 수가 감소하고, 오른쪽으로 갈 수록 1모씩 모 수가 증가한다.
+      #  (Ex. 1모 2모 *3모* 4모 5모)
+      # 4)5개 중 한개가 기준인 3모로 지목되어 공개된다.
+      # 5) 지목된 기준인 3모를 기준으로 문제로 제시되는 n모에 해당하는 사람의 이름을 올바르게 말하면 정답. 틀리면 오답.
+      
+      tofu_player = []
+      for n in range(len(self.player)):
+        tofu_player.append(self.player[n].name)  
+      
+      tofu_match = dict()
+      
+      print("-"*35)
+      print("\n두부두부두부✨ 으쌰으쌰으쌰으쌰!✨\n")
+      print("두부 게임을 시작합니다.")
+      print('\n🚨주의 사항: 문제에 알맞은 모 수에 해당하는 사람의 이름을 !똑같이! 입력하세요 (철자 틀릴 시 오답으로 간주)🚨\n')
+
       while True:
-        try:
-          flag = False
-          for i in range(len(self.player)):
-            if name in self.player[i].name:
-              flag = True
-          if flag == False:
-            raise ValueError
-        except ValueError:
-          print("잘못 입력하셨습니다. 다시 입력해주세요.")
+        if len(tofu_player) < 5:
+          for i in range(1,5-len(tofu_player)+1):
+            tofu_player.append(f'두부{i}')
+
+        random.shuffle(tofu_player)
+        print("-"*35)
+        print("순서는 다음과 같습니다")
+        print(tofu_player)
+
+        std = random.randint(0,4)
+        print(f"{tofu_player[std]}(이)가 3모입니다.")
+
+        if std < 3:
+          tofu_match[1] = tofu_player[std-2]
+          tofu_match[2] = tofu_player[std-1]
+          tofu_match[3] = tofu_player[std]
+          tofu_match[4] = tofu_player[std+1]
+          tofu_match[5] = tofu_player[std+2]
+        elif std == 3:
+          tofu_match[1] = tofu_player[std-2]
+          tofu_match[2] = tofu_player[std-1]
+          tofu_match[3] = tofu_player[std]
+          tofu_match[4] = tofu_player[std+1]
+          tofu_match[5] = tofu_player[std-3]
         else:
-          list = []
-          if cnt != 0:
-            for i in range(len(self.player)):
-              if self.player[i].name != now:
-                list.append(i)
-            a = random.randint(0, len(list) - 1)
-            name = self.player[list[a]].name
-            print(name, "좋아!")
-          cnt += 1
+          tofu_match[1] = tofu_player[std-2]
+          tofu_match[2] = tofu_player[std-1]
+          tofu_match[3] = tofu_player[std]
+          tofu_match[4] = tofu_player[std-4]
+          tofu_match[5] = tofu_player[std-3]
 
-          react = random.randint(0, 1)
-          print(reaction[react])
 
-          if react == 0: # 칵 퉤
-            for i in range(len(self.player)):
-              if now == self.player[i].name:
-                self.player[i].rejection += 1
-                if self.player[i].rejection == 3:
-                  self.player[i].drink_amount += 1
-                  self.decideTurn()
-                  exit()
-          elif react == 1: # 나도 좋아
-              now = name
+        quiz = random.randint(1,5)
+        
+        while True:  
+          if quiz == 3:
+            quiz = random.randint(1,5)
+          else:
+            break
+
+          
+        while True: #사용자 차례에서는 input으로 답 받기
+          if self.turn_player == len(self.player)-1:
+            answer = input(f"\n❗QUIZ! - {self.player[self.turn_player].name} : {quiz}모는 누구일까요?: ")
+            self.turn_player = 0
+            break
+          else:                            #컴퓨터 차례에는 랜덤으로 답 받기
+            print(f"\n❗QUIZ! - {self.player[self.turn_player].name} : {quiz}모는 누구일까요?")
+            t_f = random.randint(0,1)
+            if t_f == 0:
+                answer = tofu_match[quiz]
+            else:
+              w_answer = random.randint(1,5)
+              while True:
+                if w_answer == quiz:
+                  w_answer = random.randint(1,5)
+                else:
+                  break
+              w_answer = answer   
+            print(f"{answer}입니다.")
+            break
+            
+        if tofu_match[quiz] == answer:
+          print("\n정답!")
+          print("두부게임 계속 진행합니다")
+          self.turn_player += 1
+        else:
+          print("\n틀렸습니다!")
+          print(f'\n아 누가누가 술을 마셔😲 {self.player[self.turn_player].name}(가) 술을 마셔🤪 원~~~샷❗🧨 원샷!')
+          self.player[self.turn_player].drink_amount += 1
+          print("-"*7,"게임을 종료합니다.","-"*7)
+          self.decideTurn()
+          break
+      
                 
                 
     def game_5(self):
@@ -290,3 +345,4 @@ class Game:
 
 game = Game()
 game.game()
+
